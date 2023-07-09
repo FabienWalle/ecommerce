3599f08fd7c9ad4e711d007ec41a69e795cd72db
FabienWalle <fabienwalle13@gmail.com>
2023-07-09 21:54:57 +0200

    vidéo 13 finie

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/categories', name: 'categories_')]
@@ -17,9 +19,12 @@ class CategoriesController extends AbstractController
     }
 
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category): Response
+    public function list(Categories $category, ProductsRepository $productsRepository, Request $request): Response
     {
-        $products = $category->getProducts();
+        $page= $request->query->getInt('page',1);
+
+        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(),4);
+
         return $this->render('categories/list.html.twig', compact('category', 'products'));
     }
 }
